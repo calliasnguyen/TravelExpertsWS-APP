a96965d80d51b3db52a856526b9023c5fffb26a8
Callias Nguyen <743479@ACDM.DS.SAIT.CA>
2017-03-08 22:12:00 -0700

    Added Web Page ChrisTravelExperts

---
+++ ChrisTravelExpertsProject/src/main/java/com/calliasnguyen/calstart/Agent.java
@@ -0,0 +1,102 @@
+package com.calliasnguyen.calstart;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="agents")
+public class Agent {
+	
+	@Column(name="agentid") 
+	private Integer agentId;
+	
+	@Column(name="agtfirstname")
+	private String agtFirstName;
+	
+	@Column(name="agtmiddleinitial")
+	private String agtMiddleInitial;
+	
+	@Column(name="agtlastname")
+	private String agtLastName;
+	
+	@Column(name="agtbusphone")
+	private String agtBusPhone;
+	
+	@Column(name="agtemail")
+	private String agtEmail;
+	
+	@Column(name="agtposition")
+	private String agtPosition;
+	
+	@Column(name="agencyid")
+	private Integer agencyid;
+	
+	
+
+	public Integer getAgentId() {
+		return agentId;
+	}
+
+	public void setAgentId(Integer agentId) {
+		this.agentId = agentId;
+	}
+
+	public String getAgtFirstName() {
+		return agtFirstName;
+	}
+
+	public void setAgtFirstName(String agtFirstName) {
+		this.agtFirstName = agtFirstName;
+	}
+
+	public String getAgtMiddleInitial() {
+		return agtMiddleInitial;
+	}
+
+	public void setAgtMiddleInitial(String agtMiddleInitial) {
+		this.agtMiddleInitial = agtMiddleInitial;
+	}
+
+	public String getAgtLastName() {
+		return agtLastName;
+	}
+
+	public void setAgtLastName(String agtLastName) {
+		this.agtLastName = agtLastName;
+	}
+
+	public String getAgtBusPhone() {
+		return agtBusPhone;
+	}
+
+	public void setAgtBusPhone(String agtBusPhone) {
+		this.agtBusPhone = agtBusPhone;
+	}
+
+	public String getAgtEmail() {
+		return agtEmail;
+	}
+
+	public void setAgtEmail(String agtEmail) {
+		this.agtEmail = agtEmail;
+	}
+
+	public String getAgtPosition() {
+		return agtPosition;
+	}
+
+	public void setAgtPosition(String agtPosition) {
+		this.agtPosition = agtPosition;
+	}
+
+	public Integer getAgencyid() {
+		return agencyid;
+	}
+
+	public void setAgencyid(Integer agencyid) {
+		this.agencyid = agencyid;
+	}
+	
+
+}
---
+++ ChrisTravelExpertsProject/src/main/java/com/calliasnguyen/calstart/AgentRepository.java
@@ -0,0 +1,9 @@
+package com.calliasnguyen.calstart;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AgentRepository extends CrudRepository<Agent,Long>
+
+{
+
+}
